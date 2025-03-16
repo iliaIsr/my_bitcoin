3870bc6a533f2ece37dff59b82c62e84cd04a8cc
iliaIsr <ilil.sbl91@gmail.com>
2025-03-17 00:54:55 +0300

    need search and refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {PortfolioOverview} from "./components/portfolioOverview/PortfolioOverview";
-import {AddAssetForm} from "./components/addAsset/AddAssetForm";
+
+import {PortfolioOver} from "./components/portfolioOverview/PortfolioOver";
+
 
 function App() {
     return (
-        <div>
-            <PortfolioOverview/>
-            <AddAssetForm/>
+        <div className="globalStyle">
+            <PortfolioOver/>
         </div>
     );
 }
